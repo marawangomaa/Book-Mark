22893223ba3e43472ae7842fbc3730bba6c5433d
marawangomaa <maeawangomaa70@gmail.com>
2024-05-25 11:45:26 +0300

    validation and search

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var bNameInput = document.getElementById("bookName");
 var bUrlInput = document.getElementById("bookUrl");
 var submitBtn = document.getElementById("submitBtn");
 var table = document.getElementById("tableContant");
-
+var sInput = document.getElementById("searchInput");
 
 
 
@@ -18,9 +18,6 @@ if(localStorage.getItem("books")===null){
 function addBook(){
     if(bookName.value == "" || bookUrl.value == "")
         return;
-
-
-    // var regex = /^(https?:\/\/)?((([a-zA-Z0-9$._+!*'(),;&=-]+):?([a-zA-Z0-9$._+!*'(),;&=-]+)?@)?((\d{1,3}\.){3}\d{1,3}|([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})(:\d+)?)(\/[a-zA-Z0-9$._+!*'(),;&=-]*)*(\?[a-zA-Z0-9$._+!*'(),;&=-]*)?(#[a-zA-Z0-9$._+!*'(),;&=-]*)?$/;
     
 
     var book = {
@@ -54,7 +51,7 @@ function displayBook(){
         `;
     }
 
-    table.innerHTML=mark;
+    document.getElementById("tableContant").innerHTML=mark;
 
 }
 
@@ -74,5 +71,67 @@ function deletBook(index){
     localStorage.setItem("books", JSON.stringify(bList))
 }
 
+//! Book Name Validation function
+function validateBookName (){
+    var regex = /^[A-Z][a-z]+(?:[\s.]+[a-zA-Z]+)*$/;
+    var isValed = regex.test(bNameInput.value);
+    bNameInput.classList.remove("is-invalid","is-valid")
+
+    if(isValed){
+        // console.log("matched");
+        bNameInput.classList.add("is-valid")
+        document.getElementById("nameAlert").classList.replace("d-block","d-none")
+    }else{
+        // console.log("not matched");
+        bNameInput.classList.add("is-invalid")
+        document.getElementById("nameAlert").classList.replace("d-none","d-block")
+    }
+}
+
+//! Book Url Validation function
+function validateBookUrl (){
+    var regex = /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/;
+    var isValed = regex.test(bUrlInput.value);
+    bUrlInput.classList.remove("is-invalid","is-valid")
+    
+    if(isValed){
+        console.log("matched");
+        bUrlInput.classList.add("is-valid")
+        document.getElementById("urlAlert").classList.replace("d-block","d-none")
+    }else{
+        console.log("not matched");
+        bUrlInput.classList.add("is-invalid")
+        document.getElementById("urlAlert").classList.replace("d-none","d-block")
+    }
+}
+
+
+//! Search By Name function
+function searchByName(){
+    var term = sInput.value;
+
+
+    var mark ="";
+    for(var i =0;i<bList.length;i++){
+        if(bList[i].name.toLowerCase().includes(term.toLowerCase())){
+            mark+=`
+            <tr>
+                <td class="fw-bold">${i+1}-</td>
+                <td>${bList[i].name}</td>
+                <td>
+                    <a href="${bList[i].url}" target="_blank"><button id="visitBtn" class="btn btn-visit"><i class="fa-solid fa-eye"></i> Visit</button></a>
+                </td>
+                <td>
+                    <button onclick="deletBook()" id="deletBtn" class="btn btn-delet"><i class="fa-solid fa-trash-can"></i> Delet</button>
+                </td>
+                
+            </tr>
+        `;
+        }
+    }
+
+    document.getElementById("tableContant").innerHTML=mark;
+
+}
 
 
